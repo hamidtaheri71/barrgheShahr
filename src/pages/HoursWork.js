f49978a28d6783c30d9c8ae8caa0b010793be792
hamid.taheri <shamideloo@gmail.com>
2023-06-20 16:25:15 +0330

    addImage

--- src/pages/HoursWork.js
+++ src/pages/HoursWork.js
@@ -1,6 +1,7 @@
 export default function HoursWork() {
   return (
     <div>
+      <img style={{ height: "200px" }} alt="" src="./ax1.jpg" />
       <h2> ساعت های کاری ما</h2>
       <p>این صفحه ساعتهای کاری ماست.</p>
       <p>۹الی ۱۸</p>
